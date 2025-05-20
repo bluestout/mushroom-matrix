6492653e91cc5ab1a64b0fcc69cad7c5a5d6a108
Koranteng Anane <koranteng@bluestout.com>
2025-05-20 14:17:18 -0400

    setup up plugins

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import shopify from 'vite-plugin-shopify'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import shopify from 'vite-plugin-shopify';
+import tailwindcss from '@tailwindcss/vite';
+import cleanup from '@by-association-only/vite-plugin-shopify-clean';
+import pageReload from 'vite-plugin-page-reload';
 
 export default defineConfig({
-  plugins: [
-    shopify(),
-    tailwindcss()
-  ],
+  plugins: [shopify(), tailwindcss(), cleanup(), pageReload('/tmp/theme.update')],
   build: {
-    emptyOutDir: false
-  }
-})
+    emptyOutDir: false,
+  },
+});
